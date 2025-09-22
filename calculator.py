d5b76b9fe26a01839207902b019287602ad40ae3
skywxlker44 <dimabykov2k6@gmail.com>
2025-09-22 20:52:15 +0500

    Update calculator.py
    Handles expressions like 5 + 5 - 4 + 5*2 + 10/2

--- calculator.py
+++ calculator.py
@@ -1,1 +1,37 @@
+operators = ["+", "-", "*", "/", "//", "%"]
 
+def calculate():
+    expression = input()
+    expression = expression.split(" ")
+    for i in range(len(expression)):
+            element = expression[i]
+            if "*" in element:
+                first, second = element.split("*")
+                first, second = int(first), int(second)
+                expression[i] = str(first*second)
+            elif "//" in element:
+                first, second = element.split("//")
+                first, second = int(first), int(second)
+                expression[i] = str(first//second)
+            elif "/" in element:
+                    first, second = element.split("/")
+                    first, second = int(first), int(second)
+                    expression[i] = str(first/second)
+            elif "%" in element:
+                first, second = element.split("%")
+                first, second = int(first), int(second)
+                expression[i] = str(first%second)
+    result = float(expression[0])
+    for i in range(1, len(expression), 2):
+         if expression[i] == "+":
+              result += float(expression[i+1])
+         else:
+            result -= float(expression[i+1])
+    return result
+
+
+def main():
+    print(calculate())
+
+
+main()
