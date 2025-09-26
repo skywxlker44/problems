e7c0e72963bb7cf4162f4fe64355c061c1c08daa
skywxlker44 <dimabykov2k6@gmail.com>
2025-09-26 21:46:09 +0500

    Update calculator.py
    Handles everything except unary operators

--- calculator.py
+++ calculator.py
@@ -1,37 +1,77 @@
-operators = ["+", "-", "*", "/", "//", "%"]
+class Calculator:
+    def __init__(self, expr):
+        self.expr = expr
+        self.tokens = []
+        self.ops = {
+            "+": lambda x, y: x + y,
+            "-": lambda x, y: x - y,
+            "*": lambda x, y: x * y,
+            "/": lambda x, y: x / y,
+            "D": lambda x, y: x // y,
+            "%": lambda x, y: x % y
+            }
 
-def calculate():
-    expression = input()
-    expression = expression.split(" ")
-    for i in range(len(expression)):
-            element = expression[i]
-            if "*" in element:
-                first, second = element.split("*")
-                first, second = int(first), int(second)
-                expression[i] = str(first*second)
-            elif "//" in element:
-                first, second = element.split("//")
-                first, second = int(first), int(second)
-                expression[i] = str(first//second)
-            elif "/" in element:
-                    first, second = element.split("/")
-                    first, second = int(first), int(second)
-                    expression[i] = str(first/second)
-            elif "%" in element:
-                first, second = element.split("%")
-                first, second = int(first), int(second)
-                expression[i] = str(first%second)
-    result = float(expression[0])
-    for i in range(1, len(expression), 2):
-         if expression[i] == "+":
-              result += float(expression[i+1])
-         else:
-            result -= float(expression[i+1])
-    return result
+    def tokenize(self):
+        num = ''
+        prev_char = ''
+        for i in self.expr.replace(" ", "").replace("//", "D"):
+            if i.isdigit():
+                num += i
+            elif i in "+-" and (prev_char == "" or prev_char in "()+-*/%D"):
+                num += i
+            else:
+                if num:
+                    self.tokens.append(num)
+                    num = ""
+                self.tokens.append(i)
+            prev_char = i
+        if num:
+            self.tokens.append(num)
+
+    def eval_parentheses(self):
+        stack = []
+        for i, token in enumerate(self.tokens):
+            if token == "(":
+                stack.append(i)
+            elif token == ")":
+                start = stack[-1]
+                end = i
+
+                eval_res = self.eval_tokens(self.tokens[start+1:end])
+                self.tokens = self.tokens[:start] + [str(eval_res)] + self.tokens[end + 1:]
+
+                return self.eval_parentheses()
+        return
+
+    def eval_tokens(self, expr):
+        i = 1
+        while i < len(expr) - 1:
+            if expr[i] in ("*", "/", "D", "%"):
+                left = int(expr[i-1])
+                right = int(expr[i+1])
+                res = self.ops[expr[i]](left,right)
+                expr = expr[:i-1] + [res] + expr[i+2:]
+            else:
+                i += 2
+
+        i = 1
+        while i < len(expr) - 1:
+            if expr[i] in ("+", "-"):
+                left = int(expr[i-1])
+                right = int(expr[i+1])
+                res = self.ops[expr[i]](left,right)
+                expr = expr[:i-1] + [res] + expr[i+2:]
+            else:
+                i += 2
+        return expr[0]
+
+    def calculate(self):
+        self.tokenize()
+        self.eval_parentheses()
+        return self.eval_tokens(self.tokens)
 
 
-def main():
-    print(calculate())
-
-
-main()
+if __name__ == "__main__":
+    expression = input("Expression: ")
+    c = Calculator(expression)
+    print(c.calculate())
